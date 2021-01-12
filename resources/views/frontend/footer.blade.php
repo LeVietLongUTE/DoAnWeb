21ea3293f864b9be2d03b6d1a0486b44602a6710
LeVietLongUTE <71520285+LeVietLongUTE@users.noreply.github.com>
2021-01-12 17:11:06 +0700

    t

---
+++ resources/views/frontend/footer.blade.php
@@ -0,0 +1,117 @@
+<footer id="footer-two"><!--Footer-->
+		 <div class="footer-top section-padding center-block ">
+	        <div class="footer-dsc ">
+	            <div class="container">
+						<div class="row">
+										<div class="col-xs-12 col-sm-6 col-md-2 ">
+											<div class="single-text">
+												<div class="footer-title">
+													<h4>Liên hệ</h4>
+												</div>
+												<div class="item">
+													<div class="icon_lh">
+														<i class="fa fa-map-marker-alt"></i>
+													</div>
+													<div class="title">
+														<p>48 Cao Thắng <br> Trường đại học sư phạm kỹ thuật</p>
+													</div>
+												</div>
+												<div class="item">
+													<div class="icon_lh">
+														<i class="fas fa-phone-square-alt"></i>
+													</div>
+													<div class="title">
+														<p>(+84)383425354 <br>(+84)702076081</p>
+													</div>
+												</div>
+												<div class="item">
+													<div class="icon_lh">
+														<i class="fas fa-envelope-open-text"></i>
+													</div>
+													<div class="title">
+														<p>ute@ute12.udn.vn <br>ute123@gmail.com</p>
+													</div>
+												</div>
+											</div>
+											
+										</div>
+										<div class="col-xs-12 col-sm-6 col-md-2 ">
+											<div class="single-text">
+												<div class="footer-title">
+													<h4>Tài khoản</h4>
+												</div>
+												<div class="footer-menu">
+													<ul>
+														<p><a href="#"><i class="fa fa-caret-right"></i>Tài khoản</a></p>
+														<p><a href="#"><i class="fa fa-caret-right"></i>Giở hàng</a></p>
+														<p><a href="#"><i class="fa fa-caret-right"></i>Đăng nhập</a></p>
+														<p><a href="#"><i class="fa fa-caret-right"></i>Thanh toán</a></p>
+													</ul>
+												</div>
+											</div>
+
+										</div>
+										<div class="col-xs-12 col-sm-6 col-md-2 ">
+											<div class="single-text">
+												<div class="footer-title">
+													<h4>Danh mục</h4>
+												</div>
+												<div class="footer-menu">
+													<ul>
+														<p><a href="#"><i class="fa fa-caret-right"></i>PETS</a></p>
+														<p><a href="#"><i class="fa fa-caret-right"></i>Thức ăn</a></p>
+														<p><a href="#"><i class="fa fa-caret-right"></i>Phụ kiện</a></p>
+													</ul>
+												</div>
+											</div>
+										</div>
+
+								<div class="col-xs-12 col-sm-6 col-md-2 ">
+									<div class="single-text">
+										<div class="footer-title">
+											<div class="mxh">
+													<h2>Mạng xã hội</h2>
+												<div class="icon_mxh">
+													<a href="https://www.facebook.com/"><img src="{{('public/frontend/images/facebook_ic.jpg')}}"></a>
+												</div>
+												<div class="icon_mxh">
+													<a href="https://www.instagram.com/"><img src="{{('public/frontend/images/instagram_ic.png')}}"></a>
+												</div>
+												<div class="icon_mxh">
+													<a href="https://id.zalo.me/account?continue=https%3A%2F%2Fchat.zalo.me%2F"><img src="{{('public/frontend/images/zalos_ic.png')}}"></a>
+												</div>
+
+											</div>
+										</div>
+									</div>
+								</div>
+						
+								<!-- fanpage star -->
+									<div class="col-xs-12 col-sm-6 col-md-2 ">
+										<div class="single-text">
+											<div class="footer-title">
+												<h4>Fanpage</h4>
+											</div>
+											<div class="footer-menu">
+												<ul>
+													<iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FPetFriend-103145991754207&tabs&width=250&height=130&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="250" height="130" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+												</ul>
+											</div>
+										</div>
+									</div>
+								<!-- fanpage end -->
+						</div>
+	               
+
+					</div>
+
+				</div>
+		</div>
+	</div>
+	
+	</footer>
+	 <div class="footer-bottom">
+        	<div class="text-center">
+                    <p>&copy; Đồ Án Tốt Nghiệp Môn Web Nâng Cao 2021</p>
+            </div>
+     </div>
---
+++ resources/views/frontend/header.blade.php
@@ -0,0 +1,88 @@
+<header id="header"><!--header-->
+		<div class="header_top"><!--header_top-->
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-6">
+						<div class="contactinfo">
+							<ul class="nav nav-pills">
+								<li><a href="#"><i class="fa fa-calendar-alt"></i>  T2 - CN</a></li>
+								<li><a href="#"><i class="fa fa-clock"></i> 8:00 - 21:00</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-sm-6">
+						<div class="social-icons pull-right">
+							<ul class="nav navbar-nav">
+								<li><a href="#"><i class="fab fa-facebook"></i></a></li>
+								<li><a href="#"><i class="fab fa-twitter"></i></a></li>
+								<li><a href="#"><i class="fab fa-linkedin"></i></a></li>
+								<li><a href="#"><i class="fab fa-dribbble"></i></a></li>
+								<li><a href="#"><i class="fab fa-google-plus"></i></a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div><!--/header_top-->
+		
+		<div class="header-middle"><!--header-middle-->
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-6">
+						<div class="logo pull-left">
+							<a href="#" ><img style="width: 120px" src="{{('public/frontend/images/logo_pet.png')}}" alt="" /></a>
+						</div>
+						
+					</div>
+					<div class="col-sm-6">
+						<div class="shop-menu pull-right">
+							<ul class="nav navbar-nav">
+								<li><a href="#"><i class="fa fa-user"></i> Tài khoản</a></li>
+								<li><a href="#"><i class="fa fa-shopping-cart"></i> Giỏ hàng</a></li>
+								<li><a href="#"><i class="fa fa-lock"></i> Đăng nhập</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div><!--/header-middle-->
+
+		<div class="header-bottom" style="font-weight: 800;"><!--header-bottom-->
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-6">
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+						</div>
+						<div class="mainmenu pull-left">
+							<ul class="nav navbar-nav collapse navbar-collapse">
+								<li><a href="index.php" class="active">Trang Chủ</a></li>					
+								<li><a href="#">Mua Hàng</a>	</li> 						
+								<li><a href="#">Thanh Toán</a>	</li> 
+								
+								<li class="dropdown"><a href="#">Giới thiệu<i class="fa fa-angle-down"></i></a>
+									<ul role="menu" class="sub-menu">
+										<li><a href="#">Chó</a></li> 
+										<li><a href="#">Mèo</a></li> 
+											
+									</ul>
+								
+								</li> 
+								<li><a href="#">Liên Hệ</a>	</li> 			
+							</ul>
+						</div>
+					</div>
+					<div class="col-sm-6">
+						<div class="search_box pull-right">
+							<input type="text" placeholder="Search"/>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div><!--/header-bottom-->
+	</header><!--/header-->
