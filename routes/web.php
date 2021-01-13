033a91c71f54380695554429de2020574dd52b0a
LeVietLongUTE <71520285+LeVietLongUTE@users.noreply.github.com>
2021-01-13 21:26:09 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -22,13 +22,16 @@ Route::get('/trang-chu','HomeController@index');
 
 Route::get('/login','loginController@show_login');
 Route::get('/register','loginController@show_register');
+
 Route::get('/admin-layout','adminLayoutController@index');
 
-Route::get('/dashboard','adminLayoutController@show_dashboard');
+Route::get('/dashboard','loginController@show_dashboard');
+// Route::GET('/dashboard','loginController@loginadmin');
 
 Route::POST('/dashboard','loginController@loginadmin');
 
 Route::POST('/registeryet','loginController@register');
+
 Route::get('/logout','loginController@logout');
 
 
@@ -89,6 +92,7 @@ Route::POST('/edit-product/{product_id}','productController@edit_product');
 Route::POST('/update-product/{product_id}','productController@update_product');
 Route::GET('/delete-product/{product_id}','productController@delete_product');
 
+<<<<<<< HEAD
 
 //Gio thieu
 Route::GET('/gioithieus','HomeController@gioithieu');
@@ -97,3 +101,19 @@ Route::GET('/meo','HomeController@gioithieumeo');
 
 //lienhe
 Route::GET('/lienhe','HomeController@lienhe');
+=======
+//Người dùng
+Route::get('/add-user','loginController@show_formAddUser');
+Route::POST('/add-user','loginController@show_formAddUser');
+Route::POST('/save-user','loginController@save_User');
+
+//list user
+Route::get('/list-user','loginController@show_listUser');
+Route::POST('/list-user','loginController@show_listUser');
+//edit user
+Route::get('/edit-user/{user_id}','loginController@edit_user');
+Route::POST('/edit-user/{user_id}','loginController@edit_user');
+
+Route::POST('/update-user/{user_id}','loginController@update_user');
+Route::GET('/delete-user/{user_id}','loginController@delete_user');
+>>>>>>> origin/vanlong
